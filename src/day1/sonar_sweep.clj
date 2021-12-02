b6f1b97a00dfeed849f87fd8dab225f468d35e9a
Nuttanart Pornprasitsakul <nuttanart.p@gmail.com>
2021-12-02 12:01:19 +0700

    Refactor to use window function

--- src/day1/sonar_sweep.clj
+++ src/day1/sonar_sweep.clj
@@ -1,8 +1,38 @@
 (ns day1.sonar-sweep
   (:require [clojure.java.io :as jio]))
 
-;; Part 1
+(def input (->> (jio/reader "src/day1/input") line-seq (map parse-long)))
+
+(defn window
+  [n coll]
+  (lazy-seq
+    (when (pos? n)
+      (when-let [s (seq coll)]
+        (cons (take n s) (window n (rest s)))))))
+
 (defn count-larger
+  [numbers]
+  (->> numbers
+       (window 2)
+       (filter (comp #{2} count))
+       (filter #(apply < %))
+       count))
+
+;; Part 1
+(count-larger input)
+
+;; Part 2
+(->> input
+     (window 3)
+     (map #(apply + %))
+     count-larger)
+
+
+
+;; Original implementation
+
+;;; part1
+(defn old-count-larger
   [[start & next-vals]]
   (-> (reduce (fn [{:keys [current-val] :as acc} next-val]
                 (-> acc
@@ -13,20 +43,17 @@
               next-vals)
       :counter))
 
-(count-larger (map parse-long (line-seq (jio/reader "src/day1/input"))))
-
-
-;; Part 2
+;;; part 2
 (->> (reduce (fn [{:keys [current] :as acc} _]
                (if (seq current)
                  (-> acc
                      (update :result conj (vec (take 3 current)))
                      (update :current rest))
                  (reduced acc)))
-             {:current (map parse-long (line-seq (jio/reader "src/day1/input")))
+             {:current input
               :result []}
              (range))
      :result
      (filter (comp #{3} count))
      (map #(apply + %))
-     count-larger)
+     old-count-larger)
