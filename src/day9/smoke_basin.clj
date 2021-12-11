a3a4633d06e85c268f89304cbc8c89ba09ac6049
Nuttanart Pornprasitsakul <nuttanart.p@gmail.com>
2021-12-11 20:13:44 +0700

    Day 9, part 2

--- src/day9/smoke_basin.clj
+++ src/day9/smoke_basin.clj
@@ -5,7 +5,7 @@
 (def input-data
   (with-open [r (jio/reader "src/day9/input")]
     (->> (line-seq r)
-         (mapv (comp #(mapv parse-long %) #(str/split % #"")))
+         (map (comp #(mapv parse-long %) #(str/split % #"")))
          (into []))))
 
 (defn point->val
@@ -42,3 +42,83 @@
 (comment
   (part1 input-data)
   )
+
+(def not-neg? (comp not neg?))
+
+(defn in-range?
+  [v max-v]
+  ((every-pred not-neg? #(< % max-v)) v))
+
+(defn expand-surrounding-points
+  [[x y] [max-x max-y]]
+  (->> [[dec identity]
+        [identity dec]
+        [inc identity]
+        [identity inc]]
+       (map (fn [[fx fy]] [(fx x) (fy y)]))
+       (filter (every-pred (comp #(in-range? % max-x) first)
+                           (comp #(in-range? % max-y) second)))))
+
+(defn border? [v] (= v 9))
+(def not-border? (comp not border?))
+
+(defn find-basin-points
+  [heightmap point]
+  (let [limit [(-> heightmap first count) (count heightmap)]]
+   (reduce (fn [{:keys [search-points result-points] :as acc} _]
+             (if-let [p (first search-points)]
+               (let [acc+ (-> acc
+                              (update :checked-points conj p)
+                              (update :search-points #(into [] (next %))))]
+                 (if (-> p (point->val heightmap) not-border?)
+                   (let [next-points
+                         (->> (expand-surrounding-points p limit)
+                              (remove result-points))]
+                     (-> acc+
+                         (update :search-points #(into % next-points))
+                         (update :result-points conj p)))
+                   acc+))
+               (reduced acc)))
+           {:search-points [point]
+            :checked-points #{}
+            :result-points #{}}
+           (range))))
+
+(defn all-points
+  [heightmap]
+  (-> (let [w (count (first heightmap))
+            h (count heightmap)]
+        (for [x (range 0 w)
+              y (range 0 h)]
+          [x y]))
+      (set)))
+
+(defn find-all-basins
+  [heightmap]
+  (let [points (all-points heightmap)]
+    (-> (reduce
+          (fn [{:keys [unchecked-points] :as acc} _]
+            (if-let [p (first unchecked-points)]
+              (if (-> p (point->val heightmap) border?)
+                (update acc :unchecked-points disj p)
+                (let [{:keys [checked-points result-points]}
+                      (find-basin-points heightmap p)]
+                  (-> acc
+                      (update :unchecked-points #(apply disj % checked-points))
+                      (update :basin-points conj result-points))))
+              (reduced acc)))
+          {:unchecked-points points
+           :basin-points []}
+          (range))
+        :basin-points)))
+
+(defn part2
+  [heightmap]
+  (->> (find-all-basins heightmap)
+       (map count)
+       (sort >)
+       (take 3)
+       (apply *)))
+
+(comment
+  (part2 input-data))
