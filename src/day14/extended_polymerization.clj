e9a254297d0b8e6b5e227c52f01a628e72182981
Nuttanart Pornprasitsakul <nuttanart.p@gmail.com>
2021-12-30 07:41:18 +0700

    day 14, part 2

--- src/day14/extended_polymerization.clj
+++ src/day14/extended_polymerization.clj
@@ -9,38 +9,69 @@
       {:polymer-template polymer-template
        :pair-insertion-rules (mapv #(str/split % #" -> ") pair-insertion-rules)})))
 
-(defn rules->insertion-patterns
+(defn rules->index-updates
   [rules]
-  (into {}
-        (map (juxt (comp vec first)
-                   (juxt ffirst (comp first second) (comp second first))))
-        rules))
+  (->> rules
+       (map (juxt (comp vec first) second))
+       (map (juxt first
+                  (fn [[[a b :as z] [c]]]
+                    [[z -1] [[a c] 1] [[c b] 1]])))
+       (into {})))
+
+(defn count-pairs
+  [template]
+  (frequencies (map vector template (next template))))
+
+(defn compute-all-index-updates
+  [index index-updates]
+  (mapcat (fn [[k v]]
+            (map (juxt first (comp #(* % v) second)) (get index-updates k)))
+          index))
+
+(defn apply-updates
+  [index updates]
+  (->> (reduce #(update %1 (first %2) (fnil + 0) (second %2)) index updates)
+       (remove (comp zero? second))
+       (into {})))
+
+(defn index->char-counts
+  [index last-char]
+  (-> (->> index
+           (map (juxt ffirst second))
+           (reduce #(update %1 (first %2) (fnil + 0) (second %2)) {}))
+      (update last-char (fnil inc 0))))
+
+(defn polymerization-index
+  [index index-updates n]
+  (reduce
+    (fn [m _]
+      (let [updates (compute-all-index-updates m index-updates)]
+        (apply-updates m updates)))
+    index
+    (range n)))
 
 (defn polymerization
-  [template insertion-patterns]
-  (def template template)
-  (let [tuples (into []
-                     (map #(get insertion-patterns % %))
-                     (mapv vector template (next template)))]
-    (-> (into [] (comp (map pop) cat) (pop tuples))
-        (into (peek tuples)))))
+  [template rules n]
+  (polymerization-index (count-pairs template) (rules->index-updates rules) n))
 
-(defn polymerization-rounds
-  [template insertion-patterns n]
-  (reduce (fn [t _] (polymerization t insertion-patterns)) template (range n)))
+(defn solve
+  [{template :polymer-template rules :pair-insertion-rules} n]
+  (let [chars (-> (polymerization template rules n)
+                  (index->char-counts (last template)))
+        vs (-> chars vals sort)]
+    (- (last vs) (first vs))))
 
 (defn part1
-  [{template :polymer-template rules :pair-insertion-rules}]
-  (let [insertion-patterns (rules->insertion-patterns rules)
-        vs (-> template
-               seq
-               vec
-               (polymerization-rounds insertion-patterns 10)
-               frequencies
-               vals
-               sort)]
-    (- (last vs) (first vs))))
+  [in]
+  (solve in 10))
+
+(defn part2
+  [in]
+  (solve in 40))
 
 (comment
   (part1 input-data)
+  ; 2360
+  (part2 input-data)
+  ; 2967977072188
   )
